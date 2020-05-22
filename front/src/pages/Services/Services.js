3bf8466b9b13b1520cc62930d28514f764d24d00
Kevin Barnoin <kevbarns@mbp-k.local>
2020-05-22 22:14:26 +0200

    clean: Service component

--- front/src/pages/Services/Services.js
+++ front/src/pages/Services/Services.js
@@ -6,52 +6,50 @@ import { fetchCatalogue } from "../../redux/catalogue-services/catalogueAction";
 import Breadcumb from "../../components/Breadcrumb";
 import Card from "../../components/Card";
 import "./services.css";
+import { Redirect } from "react-router-dom";
 
-export default ({ history }) => {
-  const { items, loading, error } = useSelector((state) => state.services);
+export default () => {
   const dispatch = useDispatch();
+  const { items, loading, error } = useSelector((state) => state.services);
 
   React.useEffect(() => {
     dispatch(fetchCatalogue());
   }, [dispatch]);
 
-  return (
+  return error ? (
+    <Redirect to="/404" />
+  ) : (
     <div className="page catalogue-services">
-      {error ? (
-        history.push("/404")
-      ) : (
-        <Container className="mt-5 mb-5">
-          <Row>
-            <Breadcumb />
-            <Col xs="12" className="">
-              <h3 className="">
-                <span className="icon-esd-catalog" />
-                Catalogue des Services
-              </h3>
-              <hr className="mt-0" />
-              <p className="font-weight-light">
-                Vous trouverez ici l'ensemble des services référencés, la documentation associés et les modalités de
-                souscription
-              </p>
-            </Col>
-          </Row>
-          <Row className="ml-0 pt-2">
-            <Col xs="12" className="pl-5 border-left-custom">
-              <p className="font-weight-bold pb-3">APPRENTISSAGE</p>
-              {loading && <p>Chargement des services en cours</p>}
-              <Row>
-                {items.length > 0 &&
-                  items.map((item, key) => (
-                    <Col key={key} xs="12" sm="12" md="6" lg="4" className="my-2">
-                      <Card {...item} />
-                    </Col>
-                  ))}
-              </Row>
-              )}
-            </Col>
-          </Row>
-        </Container>
-      )}
+      <Container className="mt-5 mb-5">
+        <Row>
+          <Breadcumb />
+          <Col xs="12" className="">
+            <h3 className="">
+              <span className="icon-esd-catalog" />
+              Catalogue des Services
+            </h3>
+            <hr className="mt-0" />
+            <p className="font-weight-light">
+              Vous trouverez ici l'ensemble des services référencés, la documentation associés et les modalités de
+              souscription
+            </p>
+          </Col>
+        </Row>
+        <Row className="ml-0 pt-2">
+          <Col xs="12" className="pl-5 border-left-custom">
+            <p className="font-weight-bold pb-3">APPRENTISSAGE</p>
+            {loading && <p>Chargement des services en cours</p>}
+            <Row>
+              {items.length > 0 &&
+                items.map((item, key) => (
+                  <Col key={key} xs="12" sm="12" md="6" lg="4" className="my-2">
+                    <Card {...item} />
+                  </Col>
+                ))}
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
