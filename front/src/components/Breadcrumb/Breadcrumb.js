9802d9b0bdf138695cfd6c950367fc2da0b8ae2e
Kevin Barnoin <kevbarns@mbp-k.local>
2020-05-22 19:12:35 +0200

    fix: breadcrumb navigation

--- front/src/components/Breadcrumb/Breadcrumb.js
+++ front/src/components/Breadcrumb/Breadcrumb.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { Breadcrumb, BreadcrumbItem } from "reactstrap";
 
 import useBreadcrumbs from "use-react-router-breadcrumbs";
@@ -9,11 +10,12 @@ import "./breadcrumb.css";
 export default () => {
   const breadcrumbs = useBreadcrumbs(routes);
   const current = window.location.pathname;
+  let history = useHistory();
 
   return (
     <Breadcrumb tag="nav" listTag="div">
       {breadcrumbs.map(({ match, breadcrumb }, key) => (
-        <BreadcrumbItem key={key} tag={match.url !== current ? "a" : "div"} href={match.url}>
+        <BreadcrumbItem key={key} tag={match.url !== current ? "a" : "div"} onClick={() => history.push(match.url)} href="">
           {breadcrumb}
         </BreadcrumbItem>
       ))}
