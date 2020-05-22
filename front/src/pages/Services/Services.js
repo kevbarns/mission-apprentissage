cfba7d8d1f73bf58cee1be4fa58cf04f1d7e8d60
Kevin Barnoin <kevbarns@mbp-k.local>
2020-05-22 17:14:41 +0200

    feat: update service page

--- front/src/pages/Services/Services.js
+++ front/src/pages/Services/Services.js
@@ -1,90 +1,54 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  Col,
-  Row,
-  Container,
-  Breadcrumb,
-  BreadcrumbItem,
-  Card,
-  CardBody,
-  CardImg,
-  CardTitle,
-  CardSubtitle,
-  CardText,
-} from "reactstrap";
-import {} from "reactstrap";
+import { Col, Row, Container } from "reactstrap";
 
 import { fetchCatalogue } from "../../redux/catalogue-services/catalogueAction";
+import Breadcumb from "../../components/Breadcrumb";
+import Card from "../../components/Card";
+
 import "./services.css";
 
-const Carte = ({ nom, version, description, lien, miniature }) => (
-  <Card className="h-100">
-    <div className="d-flex justify-content-center">
-      <CardImg  src={miniature} alt="logo-service-apprentissage" className="rounded-circle service-image mt-4" />
-    </div>
-    <CardBody>
-      <CardTitle>{nom}</CardTitle>
-      <CardSubtitle>{version}</CardSubtitle>
-      <hr />
-      <CardText>{description}</CardText>
-    </CardBody>
-  </Card>
-);
-
-const Fil = () => (
-  <Breadcrumb tag="nav" listTag="div">
-    <BreadcrumbItem tag="a" href="#">
-      Home
-    </BreadcrumbItem>
-    <BreadcrumbItem href="#">Catalogue des services</BreadcrumbItem>
-  </Breadcrumb>
-);
-
-const Services = () => {
+export default ({ history }) => {
   const { items, loading, error } = useSelector((state) => state.services);
   const dispatch = useDispatch();
 
   React.useEffect(() => {
     dispatch(fetchCatalogue());
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="page catalogue-services">
-      <Container className="mt-5">
+      <Container className="mt-5 mb-5">
         <Row>
-          <Fil />
+          <Breadcumb />
           <Col xs="12" className="">
-            <h2>
-              <span className="icon-esd-catalog"></span>
-              Catalogue des services
-            </h2>
+            <h3 className="">
+              <span className="icon-esd-catalog" />
+              Catalogue des Services
+            </h3>
             <hr className="mt-0" />
-            <p>
+            <p className="font-weight-light">
               Vous trouverez ici l'ensemble des services référencés, la documentation associés et les modalités de
               souscription
             </p>
           </Col>
         </Row>
-        <Row className="">
-          <Col xs="12" className="border-left border-primary">
-            <p>APPRENTISSAGE</p>
-            {loading ? <p>Chargement en cours</p> : null}
+        <Row className="ml-0 pt-2">
+          <Col xs="12" className="pl-5 border-left-custom">
+            <p className="font-weight-bold pb-3">APPRENTISSAGE</p>
+            {loading && <p>Chargement en cours</p>}
+            {error && history.push("/404")}
             <Row>
-              {items
-                ? items.map((item) => (
-                    <Col xs="6" sm="4" className="my-2">
-                      <Carte {...item} />
-                    </Col>
-                  ))
-                : null}
+              {items &&
+                items.map((item) => (
+                  <Col xs="12" sm="12" md="6" lg="4" className="my-2">
+                    <Card {...item} />
+                  </Col>
+                ))}
             </Row>
-            {/* {items.map((item) => {})} */}
           </Col>
         </Row>
       </Container>
     </div>
   );
 };
-
-export default Services;
