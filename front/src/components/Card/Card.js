1424eddd159429454fc2fa3324dd3d44a84b4449
Kevin Barnoin <kevbarns@mbp-k.local>
2020-05-25 00:51:25 +0200

    fix: if not link available

--- front/src/components/Card/Card.js
+++ front/src/components/Card/Card.js
@@ -6,18 +6,20 @@ import "./card.css";
 export default ({ nom, version, description, lien, miniature }) => (
   <Card className="h-100 border-0">
     <div className="d-flex justify-content-center">
-      <CardImg src={miniature} alt="logo-service-apprentissage" className="rounded-circle shadow-sm bg-white service-image mt-4" />
+      <CardImg
+        src={miniature}
+        alt="logo-service-apprentissage"
+        className="rounded-circle shadow-sm bg-white service-image mt-4"
+      />
     </div>
     <CardBody>
       <CardTitle className="font-weight-normal">{nom}</CardTitle>
-      <CardSubtitle className="font-weight-light pb-1 d-inline-flex mb-3">
-        {version}
-      </CardSubtitle>
+      <CardSubtitle className="font-weight-light pb-1 d-inline-flex mb-3">{version}</CardSubtitle>
       <CardText className="font-weight-light">{description}</CardText>
     </CardBody>
     <CardFooter className="text-center">
-      <Button className="mt-2" href={lien} target="_blank">
-        Accéder au Service
+      <Button className="mt-2" href={lien} target="_blank" disabled={lien ? false : true}>
+        {lien ? "Accéder au Service" : "En cours de développement"}
       </Button>
     </CardFooter>
   </Card>
